ab0d81fb2b59a24d8ac3bda54cf010834bc18ed0
Jiri Bakker <jiribakker@bol.com>
2022-02-06 21:28:01 +0100

    Fix input cycle

--- src/main/kotlin/solver/WordleSolver.kt
+++ src/main/kotlin/solver/WordleSolver.kt
@@ -22,26 +22,27 @@ object WordleSolver {
         val guessHistory = mutableListOf<Pair<Guess, Result>>()
         var candidates = candidateFilters.associateWith { fiveLetterWords }
 
-        return sequence {
-           do {
-                val guesses = candidates.map { (_, words) ->
-                    candidateSelector.selectBest(words)
-                }
-                yield(guesses)
+        val inputProvider = input.iterator()
 
-                val (guess, result) = input.iterator().next()
+        fun getGuesses() = candidates.map { (_, words) ->
+            candidateSelector.selectBest(words)
+        }
+
+        return sequence {
+            yield(getGuesses())
+
+            do {
+                val (guess, result) = inputProvider.next()
                 // TODO validate input?
 
-                when {
-                    else -> {
-                        guessHistory.add(guess to result)
+                guessHistory.add(guess to result)
 
-                        candidates = candidateFilters.associateWith { filter ->
-                            filter.filterCandidates(fiveLetterWords, guessHistory)
-                        }
-                    }
+                candidates = candidateFilters.associateWith { filter ->
+                    filter.filterCandidates(fiveLetterWords, guessHistory)
                 }
-            } while (input.iterator().hasNext())
+
+                yield(getGuesses())
+            } while (inputProvider.hasNext())
         }
     }
 }
